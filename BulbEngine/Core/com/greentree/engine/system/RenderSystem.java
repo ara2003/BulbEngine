39a896007636c025f53c759b12cfa8cfae17d1eb
arseny <arseny.latyshev@gmail.com>
2020-12-27 04:56:54 +0300

    works version with renderSystem

--- BulbEngine/Core/com/greentree/engine/system/RenderSystem.java
+++ BulbEngine/Core/com/greentree/engine/system/RenderSystem.java
@@ -1,5 +1,11 @@
 package com.greentree.engine.system;
 
+import java.util.List;
+
+import com.greentree.engine.component.Camera;
+import com.greentree.engine.component.RendenerComponent;
+import com.greentree.engine.opengl.rendener.Renderer;
+import com.greentree.engine.opengl.rendener.SGL;
 import com.greentree.engine.system.util.GameSystem;
 
 
@@ -10,9 +16,17 @@ import com.greentree.engine.system.util.GameSystem;
 public class RenderSystem extends GameSystem {
 	
 	private static final long serialVersionUID = 1L;
+	private static SGL GL = Renderer.get();
 	
 	@Override
 	public void execute() {
+		List<Camera> cameras = getComponents(Camera.class);
+		List<RendenerComponent> rendeners = getComponents(RendenerComponent.class);
+		
+		for(Camera camera : cameras)
+			for(RendenerComponent rendener : rendeners) if(camera.mustDraw(rendener)){
+				rendener.draw(GL);
+			}
 		
 	}
 	
